1d98c87d7906580f3677220d8b55528834d274e2
olgababikova <100796016+olgababikova@users.noreply.github.com>
2022-10-12 23:35:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const API_URL ='https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        filteredProducts: [],
+        searchLine: '',
+        isVisibleCart: false,
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        basket:[],
+    },
+
+    methods: {
+        makeGETRequest(url, callback){
+            
+            var xhr;
+            if (window.XMLHttpRequest) {
+                // Chrome, Mozilla, Opera, Safari
+                xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                // Internet Explorer
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            
+            xhr.onreadystatechange = function () { 
+                if (xhr.readyState === 4) {      
+                    callback(xhr.responseText); 
+                }
+            }
+            
+            xhr.open('GET', url, true);
+            xhr.send();
+        },
+        
+        search(searchLine){
+            const regex = new RegExp(searchLine, 'i');
+            this.filteredProducts =  this.products.filter(p => regex.test(p.product_name));
+            
+            //this.render();
+        },
+
+        put(product) {
+            //добавление в корзину
+        }
+
+    },
+
+    mounted() {
+        this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) =>{
+            this.products = JSON.parse(goods);
+            this.filteredProducts = JSON.parse(goods);
+            //cb();
+        })
+    },
+    
+    computed: {
+
+    }
+});
+
+
+    
